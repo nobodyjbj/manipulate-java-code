928bdc72c5cd88e856a45ce6931d1ada5c420ae2
nobodyjbj <nobody.jbj@gmail.com>
2022-04-05 18:02:53 +0900

    reflection

---
+++ demo-spring-di/src/test/java/me/example/demospringdi/DemoSpringDiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.example.demospringdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
